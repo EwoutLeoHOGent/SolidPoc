db84b55cb48ee59324d89a2f38564f9c4a516d47
EwoutLeoHOGent <ewout.leo@student.hogent.be>
2021-11-27 15:52:51 +0100

    Add pod provider choice

--- solid-poc/src/index.js
+++ solid-poc/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import SolidPoc from "./App.jsx";
+import SolidPoc from "./App.js";
 
 ReactDOM.render(
   <React.StrictMode>
